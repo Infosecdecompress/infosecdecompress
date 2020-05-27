0ea3595a9f08370dd001ffedcf4b3664df584cdb
Kiselyov, Anton6 <a6.kiselyov@itransition.com>
2020-05-27 16:27:01 +0300

    added Dark Mode functionality

---
+++ blog/src/components/Sidebar/Switcher/Switcher.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './Switcher.module.scss';
+
+
+
+const Switcher = () => {
+  let isDarkModeOn = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  window.localStorage.setItem('darkMode', isDarkModeOn);
+  const bodyEl = document.getElementsByTagName('body')[0];
+
+  function TurnDarkMode() {
+    isDarkModeOn ? bodyEl.classList.remove('dark') : bodyEl.classList.add('dark');
+    isDarkModeOn = !isDarkModeOn;
+    window.localStorage.setItem('darkMode', isDarkModeOn);
+  }
+
+  return (
+    <div className={styles['switch__container']}>
+      <span className={styles['switch__text']}>Night mode</span>
+      <label className={styles['switch']}>
+        {isDarkModeOn
+          ? <input id='DarkModeSwitcher' type='checkbox' checked />
+          : <input id='DarkModeSwitcher' type='checkbox' />}
+        <span className={`${styles['slider']}`} onClick={TurnDarkMode}/>
+      </label>
+    </div>
+  )
+}
+
+export default Switcher
