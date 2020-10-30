97659221fe7d282657ee47a641950a17d5dbf705
Chi-Heng Hung <jeffreyhung@Hotmail.com.tw>
2020-10-30 16:29:42 -0500

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,6 +6,7 @@ import Copyright from './Copyright';
 import Menu from './Menu';
 import styles from './Sidebar.module.scss';
 import { useSiteMetadata } from '../../hooks';
+import Search from "../SearchContainer";
 
 type Props = {
   isIndex?: boolean,
