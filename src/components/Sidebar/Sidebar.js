439de7796d0d53c2483c64b7d1640e54597f6f76
Chi-Heng Hung <jeffreyhung@Hotmail.com.tw>
2020-11-02 15:57:07 -0600

    Add search bar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -21,6 +21,7 @@ const Sidebar = ({ isIndex }: Props) => {
         <Author author={author} isIndex={isIndex} />
         <Menu menu={menu} />
         <Contacts contacts={author.contacts} />
+        <Search />
         <Copyright copyright={copyright} />
       </div>
     </div>
