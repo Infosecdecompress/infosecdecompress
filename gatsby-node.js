c87e61900d92b4d68d6ccf04ed02ad38af6998c9
Chi-Heng Hung <jeffreyhung@Hotmail.com.tw>
2020-11-25 16:29:00 -0600

    Add customize schema to graphql

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 'use strict';
 
+exports.createSchemaCustomization = require('./gatsby/create-schema-customization');
 exports.createPages = require('./gatsby/create-pages');
 exports.onCreateNode = require('./gatsby/on-create-node');
