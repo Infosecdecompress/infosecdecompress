3059f72e55e642707c0943e70f38515514d33c6a
Jeffrey Hung <17494876+Jeffreyhung@users.noreply.github.com>
2021-05-22 12:35:50 -0500

    Add license info to page footer

--- src/components/Post/Author/Author.tsx
+++ src/components/Post/Author/Author.tsx
@@ -9,6 +9,15 @@ const Author = () => {
   return (
     <div className={styles['author']}>
       <p className={styles}><center>
+        除非特別註記，資安解壓縮網站內容皆以
+        <a 
+          href="http://creativecommons.org/licenses/by-sa/4.0/"
+          rel="noopener noreferrer"
+        >
+          CC BY-SA 4.0
+        </a>
+        方式授權
+        <br/>
         對於這個節目有什麼想法，或想要聽什麼內容都歡迎到 
         <a
           href="/pages/contacts"
@@ -16,7 +25,7 @@ const Author = () => {
         >
           聯絡資訊
         </a>
-        告訴我們。
+        告訴我們
       </center></p>
     </div>
   );
