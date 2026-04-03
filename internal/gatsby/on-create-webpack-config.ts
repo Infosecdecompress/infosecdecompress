92c6e621ad290cae13880f3b04a73780d8aa5086
jeffreyhung <17494876+Jeffreyhung@users.noreply.github.com>
2026-04-03 15:01:24 -0700

    fix deploy

--- internal/gatsby/on-create-webpack-config.ts
+++ internal/gatsby/on-create-webpack-config.ts
@@ -7,7 +7,7 @@ import { compilerOptions } from "../../tsconfig.json";
 
 const onCreateWebpackConfig = (
   (options: Pick<CompilerOptions, "paths">) =>
-  ({ actions }: CreateWebpackConfigArgs) => {
+  ({ actions, getConfig }: CreateWebpackConfigArgs) => {
     actions.setWebpackConfig({
       resolve: {
         alias: Object.entries(options.paths || []).reduce(
@@ -19,6 +19,13 @@ const onCreateWebpackConfig = (
         ),
       },
     });
+
+    const config = getConfig();
+    config.plugins = config.plugins.filter(
+      (plugin: { constructor: { name: string } }) =>
+        plugin.constructor.name !== "ESLintWebpackPlugin",
+    );
+    actions.replaceWebpackConfig(config);
   }
 )(compilerOptions);
 
